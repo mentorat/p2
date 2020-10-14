0f42e3d0c904a45af974269a9261ceab5eca0002
Pascaline M <pascaline.martres@gmail.com>
2020-10-14 15:29:25 -0400

    meilleure mise en page

--- partie3.py
+++ partie3.py
@@ -5,16 +5,16 @@ import os
 url = "http://books.toscrape.com/" 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
-liste = soup.find(class_= "nav nav-list")
-li =[]
-li = liste.find_all("li")
+list_category = soup.find(class_= "nav nav-list")
+li = list_category.find_all("li")
 category_url =[url + item.find("a", href= True).get("href") for item in  li]
+print(category_url)
 
-category = pd.DataFrame(
-    {
-        "CATEGORY URL": category_url,
-    })
-print(category)
+#category = pd.DataFrame(
+ #   {
+  #      "CATEGORY URL": category_url,
+   # })
+#print(category)"""
 
 #category.to_csv("accueil.to_csv")
 os.system("pause") 
