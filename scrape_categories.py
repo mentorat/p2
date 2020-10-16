1d2d112bc79ff8f01797153660088ddb0c550341
Pascaline M <pascaline.martres@gmail.com>
2020-10-16 19:43:35 -0400

    creation fichier des urls des categories a partir de la page d accueil

--- scrape_categories.py
+++ scrape_categories.py
@@ -8,16 +8,22 @@ from bs4 import BeautifulSoup
 import numpy as np
 
 import time
+categories_url = []
 
 url = "http://books.toscrape.com/" 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 list_category = soup.find(class_= "nav nav-list")
 li = list_category.find_all("li")
-category_url =[url + item.find("a", href= True).get("href") for item in  li]
-print(category_url)
+for item in li :
+    category_url = url + item.find("a", href= True).get("href")
+    categories_url.append(category_url)
+time.sleep(1)
 
-# si on veut creer une page CSV des categories:
+with open("urlcategories.txt", "w") as file :
+    for category_url in categories_url :
+        file.write(str(category_url) + "\n")
 
+# si on veut creer une page CSV des categories:        
 #category.to_csv("accueil.to_csv")
 os.system("pause") 
