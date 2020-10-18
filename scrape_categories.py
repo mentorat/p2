b1add61ec63667bc7f35eea1837fb90e56f718b7
Pascaline M <pascaline.martres@gmail.com>
2020-10-18 19:12:56 -0400

    ajout module csv

--- scrape_categories.py
+++ scrape_categories.py
@@ -8,22 +8,22 @@ from bs4 import BeautifulSoup
 import numpy as np
 
 import time
-categories_url = []
+
+import csv
+
 
 url = "http://books.toscrape.com/" 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 list_category = soup.find(class_= "nav nav-list")
 li = list_category.find_all("li")
-for item in li :
-    category_url = url + item.find("a", href= True).get("href")
-    categories_url.append(category_url)
-time.sleep(1)
+category_name = [item.find("a").text for item in li] # trouver comment obtenir  QUE le texte sans mise en page
+category_url =[url + item.find("a", href= True).get("href") for item in  li]
 
-with open("urlcategories.txt", "w") as file :
-    for category_url in categories_url :
-        file.write(str(category_url) + "\n")
 
-# si on veut creer une page CSV des categories:        
-#category.to_csv("accueil.to_csv")
+#creation fichier CSV des categories + url :
+
+with open("categories.csv", "w") as file :
+    file.write("CATEGORY :, URL CATEGORY :\n")
+
 os.system("pause") 
