4d799ff74c02c0062fb73b0f3c1ba602d90d3ac6
Pascaline M <pascaline.martres@gmail.com>
2020-10-20 16:38:09 -0400

    creation dictionnaire book et fichier csv au nom du livre

--- scrape_product.py
+++ scrape_product.py
@@ -7,50 +7,52 @@ from bs4 import BeautifulSoup
 
 import numpy as np
 
-import time
+import csv
 
-#SCRAPER LES INFOS D UN LIVRE  SUR SA PAGE 
+books =[]
+book = {}
 
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
 #PRODUCT PAGE URL :
-product_page_url = url
+book["product_page_url"] = url
 
 #TITLE :
 title = soup.h1.string
+book["title"] = soup.h1.string
 
 #UPC+ PRICE inc Tx + PRICE ex TAX + NUMBER AVAILABLE :
 table_striped = soup.find(class_="table table-striped")
 td = table_striped.find_all("td")
-universal_product_code = td[0].string
-price_including_tax = td[3].string
-price_excluding_tax = td[2].string
-number_available = td[5].string
+book["universal_product_code"] = td[0].string
+book["price_including_tax"] = td[3].string
+book["price_excluding_tax"] = td[2].string
+book["number_available"] = td[5].string
 
 #PRODUCT DESCRIPTION :
 p = soup.find_all("p")
-product_description = p[3].string
+book["product_description"] = p[3].string
 
 #CATEGORY :
 # trouver une maniere + courte pour category
 breadcrumb = soup.find(class_= "breadcrumb")
 a = breadcrumb.find_all("a")
-category = a[2].string
+book["category"] = a[2].string
 
 #REVIEW_RATING :
 stars = soup.find(class_="col-sm-6 product_main")
-review_rating = len(stars.find_all(class_= "icon-star"))
-
+book["review_rating"] = len(stars.find_all(class_= "icon-star"))
 #IMAGE :
 image = soup.find("img")
-image_url = url + image["src"]
+book["image_url"] = url + image["src"]
 
 #si on veut creer une page CSV pour le livre :
-with open("product.csv", "w") as file :
-    file.write("PRODUCT PAGE URL : , UPC : , TITLE : , PRICE INCLUDING TAX : , PRICE EXCLUDING TAX : , NUMBER AVAILABLE : , PRODUCT DESCRIPTION : , CATEGORY : , REVIEW RATING : , IMAGE URL :\n")
-    file.write(str(product_page_url) + universal_product_code + title + price_including_tax + price_excluding_tax + number_available + product_description + category + str(review_rating) + str(image_url) +"\n")
+with open(str(title)+".csv", "w", newline="", encoding="utf8") as file :
+    writer = csv.DictWriter(file, fieldnames=book.keys(), delimiter=";")
+    writer.writeheader()
+    writer.writerow(book)
 
 os.system("pause") 
 
