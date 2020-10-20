1a059fcbcdaa833fca6669b49cffb94f4ceb57a4
Pascaline M <pascaline.martres@gmail.com>
2020-10-20 16:39:10 -0400

    creation fichier csv categories

--- scrape_categories.py
+++ scrape_categories.py
@@ -7,23 +7,21 @@ from bs4 import BeautifulSoup
 
 import numpy as np
 
-import time
-
 import csv
 
-
+categories_urls =[]
 url = "http://books.toscrape.com/" 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 list_category = soup.find(class_= "nav nav-list")
 li = list_category.find_all("li")
-category_name = [item.find("a").text for item in li] # trouver comment obtenir  QUE le texte sans mise en page
-category_url =[url + item.find("a", href= True).get("href") for item in  li]
-
+for item in li :
+    category_url= url + item.find("a", href= True).get("href")
+    categories_urls.append(category_url)
 
 #creation fichier CSV des categories + url :
-
-with open("categories.csv", "w") as file :
-    file.write("CATEGORY :, URL CATEGORY :\n")
-
+with open("categories.csv", "w",) as csvfile :
+    for category_url in categories_urls :
+        csvfile.write(str(category_url)+"\n")
+      
 os.system("pause") 
