68001b484246f4045d64ee9abd4ddd2a767dfd14
Pascaline M <pascaline.martres@gmail.com>
2020-11-01 18:36:40 -0500

    derniere mise a jour

--- SCRIPT.py
+++ SCRIPT.py
@@ -17,13 +17,13 @@ path = "BooksToScrape"
 category_url = main.get_categories_urls(url, path)
 
 print("\n\n" +"WELCOME TO BOOKS To SCRAPE"+ "\n\n")
-
-continue_scrape = True
 print("A folder BookToScrape has been created. Open categories.csv and copy the category's url you want to scrape"+ "\n\n")
+continue_scrape = True
+
 while continue_scrape:
     choice = input("Which category would you like to scrape ?"+"\n\n")
     choice = str(choice)
-    print("Loading ... "+"\n")
+    print("\n"+"Loading ... "+"\n")
     if choice in category_url:
         main.get_books_details(choice)
         more_scrape = input("Would you like to scrape an other category ? Y/N \n")
